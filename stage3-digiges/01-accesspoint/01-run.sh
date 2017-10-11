4c0b16b3940a966cf9d202d1472584ade292e883
Reto Schneider <code@reto-schneider.ch>
2017-10-11 23:52:24 +0200

    Prevent wpa_supplicant from configuring wlan0

--- stage3-digiges/01-accesspoint/01-run.sh
+++ stage3-digiges/01-accesspoint/01-run.sh
@@ -5,3 +5,5 @@ install -m 644 files/wlan0 ${ROOTFS_DIR}/etc/network/interfaces.d/wlan0
 install -m 644 files/hostapd.conf ${ROOTFS_DIR}/etc/hostapd/hostapd.conf
 sed -i 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/' ${ROOTFS_DIR}/etc/default/hostapd
 
+# Prevent wpa_supplicant from grabbing our wlan0 interface
+echo "disabled=1" >> ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf
