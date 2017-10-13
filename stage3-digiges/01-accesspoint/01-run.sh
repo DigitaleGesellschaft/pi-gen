71b37daa0768ed6d708d80c8b26823a30ae110a1
Reto Schneider <code@reto-schneider.ch>
2017-10-14 01:51:03 +0200

    Add comment to iptables section

--- stage3-digiges/01-accesspoint/01-run.sh
+++ stage3-digiges/01-accesspoint/01-run.sh
@@ -8,6 +8,7 @@ sed -i 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/' ${ROOTFS_
 # Prevent wpa_supplicant from grabbing our wlan0 interface
 echo "disabled=1" >> ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf
 
+# Install traffic forwarding rules for iptables
 install -m 644 files/30-ipforward.conf ${ROOTFS_DIR}/etc/sysctl.d/30-ipforward.conf
 install -m 644 files/iptables.up.rules ${ROOTFS_DIR}/etc/iptables.up.rules
 install -m 755 files/iptables ${ROOTFS_DIR}/etc/network/if-pre-up.d/iptables
