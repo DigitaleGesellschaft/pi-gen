41399603e081c76a621ce20b4e176fc08bedb940
Reto Schneider <code@reto-schneider.ch>
2017-10-15 14:02:06 +0200

    Remove superfluous newlines

--- stage3-digiges/00-unattended-upgrades/00-run.sh
+++ stage3-digiges/00-unattended-upgrades/00-run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash -e
 
 install -m 644 files/20auto-upgrades ${ROOTFS_DIR}/etc/apt/apt.conf.d/20auto-upgrades
-
--- stage3-digiges/01-disable-rpi3-wifi/00-run.sh
+++ stage3-digiges/01-disable-rpi3-wifi/00-run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash -e
 
 install -m 644 files/blacklist-rpi3-wifi.conf ${ROOTFS_DIR}/etc/modprobe.d/blacklist-rpi3-wifi.conf
-
