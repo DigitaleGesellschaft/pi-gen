bbfa796982215b6f7162ed385b2c8e2cde986915
Reto Schneider <code@reto-schneider.ch>
2017-10-15 13:18:39 +0200

    Print pi-gen manipulations on console

--- configure-dig-on-pi.sh
+++ configure-dig-on-pi.sh
@@ -13,12 +13,12 @@ cp -ra ${BASE_DIR}/stage3-digiges ${BASE_DIR}/stage3
 echo "Adapt image name"
 echo "IMG_NAME=DigOnPi" > ${BASE_DIR}/config
 
-# Speed up build: Remove build tools from image
+echo "Speed up build: Remove build tools from image"
 sed -i "/build-essential manpages-dev python bash-completion gdb pkg-config/d" ${BASE_DIR}/stage2/01-sys-tweaks/00-packages
 sed -i "/libraspberrypi-dev libraspberrypi-doc libfreetype6-dev/d" ${BASE_DIR}/stage2/01-sys-tweaks/00-packages
 
-# Remove attack surface: Remove service avahi-daemon
+echo "Remove attack surface: Remove service avahi-daemon"
 sed -i "/avahi-daemon/d" ${BASE_DIR}/stage2/01-sys-tweaks/00-packages
 
-# Speed up build: Remove NOOBS exports
+echo "Speed up build: Remove NOOBS exports"
 rm -f ${BASE_DIR}/stage2/EXPORT_NOOBS
