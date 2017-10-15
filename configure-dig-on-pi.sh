79301d950de8eebdba952d55c47aabbd1e914527
Reto Schneider <code@reto-schneider.ch>
2017-10-15 14:22:07 +0200

    Improve wording in and organization of configuration script

--- configure-dig-on-pi.sh
+++ configure-dig-on-pi.sh
@@ -4,24 +4,27 @@ set -e
 
 export BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-echo "Delete stages 3 to 5"
+echo "Set up DigiGes image"
+echo " - Delete stages 3 to 5"
 rm -rf ${BASE_DIR}/stage{3,4,5}
 
-echo "Replace stage3 with the DigiGes scripts"
+echo " - Replace stage 3 with our scripts"
 cp -ra ${BASE_DIR}/stage3-digiges ${BASE_DIR}/stage3
 
-echo "Adapt image name"
+echo " - Adapt image name"
 echo "IMG_NAME=DigOnPi" > ${BASE_DIR}/config
 
-echo "Speed up build: Remove build tools from image"
+echo "Speed up build"
+echo " - Remove build tools from image"
 sed -i "/build-essential manpages-dev python bash-completion gdb pkg-config/d" ${BASE_DIR}/stage2/01-sys-tweaks/00-packages
 sed -i "/libraspberrypi-dev libraspberrypi-doc libfreetype6-dev/d" ${BASE_DIR}/stage2/01-sys-tweaks/00-packages
 
-echo "Remove attack surface: Remove service avahi-daemon"
-sed -i "/avahi-daemon/d" ${BASE_DIR}/stage2/01-sys-tweaks/00-packages
-
-echo "Speed up build: Remove NOOBS exports"
+echo " - Do not export NOOBS"
 rm -f ${BASE_DIR}/stage2/EXPORT_NOOBS
 
-echo "Speed up build: Remove locales from image"
+echo " - Do not install locales"
 rm -rf ${BASE_DIR}/stage0/01-locale
+
+echo "Shrink attack surface"
+echo " - Do not install service avahi-daemon"
+sed -i "/avahi-daemon/d" ${BASE_DIR}/stage2/01-sys-tweaks/00-packages
